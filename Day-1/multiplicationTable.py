cdde48c5e5dc5d51c868eb50f39d7f5be1185740
mdabutalebmojumder <mdabutalebmojumder@gmail.com>
2023-01-18 12:30:15 +0600

    my msg

---
+++ Day-1/multiplicationTable.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(n, 'x', i, '=', n * i)
+    
+'''
+a = '5'
+b = '3'
+a + b = '5' + '3' = '53'
+'''
+# a = input()
+# b = input()
+# print(a + b)
